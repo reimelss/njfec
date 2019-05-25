22cc09c10b2b6a19fd313116444e32b628156d37
dev <vishal140591@gmail.com>
2019-05-25 09:16:39 +0530

    child theme configuration

---
+++ wp-content/themes/designfolio-child/footer.php
@@ -0,0 +1,14 @@
+    <?php PC_Hooks::pc_after_opening_footer_tag(); /* Framework hook wrapper */ ?>
+    <?php get_sidebar( 'footer' ); // Adds support for the four footer widget areas ?>
+
+
+	</footer>
+
+</div><!-- #body-container -->
+<?php wp_footer(); ?>
+
+<?php PC_Hooks::pc_after_closing_footer_tag(); /* Framework hook wrapper */ ?>
+
+
+</body>
+</html>
