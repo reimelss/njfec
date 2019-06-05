1d8408892332ed93fd75995926cb667a6f748c16
dev <vishal140591@gmail.com>
2019-06-06 01:01:05 +0530

    sponser section creation

--- wp-content/themes/designfolio-child/footer.php
+++ wp-content/themes/designfolio-child/footer.php
@@ -1,8 +1,10 @@
     <?php PC_Hooks::pc_after_opening_footer_tag(); /* Framework hook wrapper */ ?>
 
-<div id="container" class="top_footer" style="clear: both;">
-	<div class="sponserSection">
-		<?php echo do_shortcode('[logooos columns="6" itemsheightpercentage="0.60" backgroundcolor="transparent" layout="slider" category="46" orderby="date" order="DESC" marginbetweenitems="25px" tooltip="enabled" responsive="enabled" grayscale="disabled" border="disabled" borderradius="logooos_no_radius" autoplay="true" transitioneffect="scroll" scrollduration="1000" pauseduration="9000" buttonsbgcolor="#FFFFFF" buttonsarrowscolor="darkgray" ]'); ?>
+<div class="top_footer" style="clear: both;">
+	<div id="container">
+		<div class="sponserSection">
+			<?php echo do_shortcode('[logooos columns="6" itemsheightpercentage="0.60" backgroundcolor="transparent" layout="slider" category="46" orderby="date" order="DESC" marginbetweenitems="25px" tooltip="enabled" responsive="enabled" grayscale="disabled" border="disabled" borderradius="logooos_no_radius" autoplay="true" transitioneffect="scroll" scrollduration="1000" pauseduration="9000" buttonsbgcolor="#FFFFFF" buttonsarrowscolor="darkgray" ]'); ?>
+		</div>
 	</div>
 </div>
 
@@ -14,7 +16,7 @@
 
 </div><!-- #body-container -->
 <style type="text/css">
-	.sponserSection {
+	.top_footer {
 	    background: #fff;
 	}
 	.sponserSection div.logooos div.logooos_item a {
