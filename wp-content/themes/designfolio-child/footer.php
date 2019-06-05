1a11f20bb528923f87a5e7dd9fc2d1f222ecadca
dev <vishal140591@gmail.com>
2019-06-06 00:38:42 +0530

    sponser section creation

--- wp-content/themes/designfolio-child/footer.php
+++ wp-content/themes/designfolio-child/footer.php
@@ -1,4 +1,10 @@
     <?php PC_Hooks::pc_after_opening_footer_tag(); /* Framework hook wrapper */ ?>
+
+<div class="top_footer" style="clear: both;">
+	<h1>Bottom Footer</h1>
+</div>
+
+
     <?php get_sidebar( 'footer' ); // Adds support for the four footer widget areas ?>
 
 
