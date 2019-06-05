b15a61d3a15dc51599e73c447fd259774c9911bd
dev <vishal140591@gmail.com>
2019-06-06 00:32:56 +0530

    sponser section creation

--- wp-content/themes/designfolio-child/footer.php
+++ wp-content/themes/designfolio-child/footer.php
@@ -1,3 +1,5 @@
+    <h1>Test</h1>
+
     <?php PC_Hooks::pc_after_opening_footer_tag(); /* Framework hook wrapper */ ?>
     <?php get_sidebar( 'footer' ); // Adds support for the four footer widget areas ?>
 
@@ -5,9 +7,6 @@
 	</footer>
 
 </div><!-- #body-container -->
-<div class="sponserSection">
-	<?php echo do_shortcode('[logooos columns="3" itemsheightpercentage="0.75" backgroundcolor="transparent" layout="grid" category="46" orderby="date" order="DESC" marginbetweenitems="5px" tooltip="enabled" responsive="enabled" grayscale="disabled" border="enabled" bordercolor="#DCDCDC" borderradius="logooos_no_radius" hovereffect="effect1" hovereffectcolor="#DCDCDC" ]'); ?>
-</div>
 <?php wp_footer(); ?>
 
 <?php PC_Hooks::pc_after_closing_footer_tag(); /* Framework hook wrapper */ ?>
