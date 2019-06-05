ee2aca2e61bf275ce3e5445453aed8a956a88934
dev <vishal140591@gmail.com>
2019-06-06 01:03:58 +0530

    sponser section creation

--- wp-content/themes/designfolio-child/footer.php
+++ wp-content/themes/designfolio-child/footer.php
@@ -2,6 +2,7 @@
 
 <div class="top_footer" style="clear: both;">
 	<div id="container">
+		<h2><span style="font-size: 28px; font-weight: bold; line-height: 32px;color: #df272a;">Sponsors</span></h2>
 		<div class="sponserSection">
 			<?php echo do_shortcode('[logooos columns="6" itemsheightpercentage="0.60" backgroundcolor="transparent" layout="slider" category="46" orderby="date" order="DESC" marginbetweenitems="25px" tooltip="enabled" responsive="enabled" grayscale="disabled" border="disabled" borderradius="logooos_no_radius" autoplay="true" transitioneffect="scroll" scrollduration="1000" pauseduration="9000" buttonsbgcolor="#FFFFFF" buttonsarrowscolor="darkgray" ]'); ?>
 		</div>
